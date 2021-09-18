7e1146d282254ee41a256a98c243584f20d2c04c
Edu Yubero <edu.yubero@gmail.com>
2021-09-18 18:51:58 +0200

    Tables migrated and seed prepared

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+contacts = Contact.create([
+    {
+        first_name: "Eduardo",
+        last_name: "Sanchez",
+        email: "edu.yubero@gmail.com",
+        phone: "676412190"
+    },
+    {
+        first_name: "Electra",
+        last_name: "Tapanari",
+        email: "etapa@gmail.com",
+        phone: "682412255"
+    },
+    {
+        first_name: "Esteban",
+        last_name: "Yubero",
+        email: "yubero.esteba@gmail.com",
+        phone: "5124123123"
+    },
+    {
+        first_name: "Maria",
+        last_name: "Cruz",
+        email: "cruzesteve@gmail.com",
+        phone: "62912312312"
+    }            
+])
+
+changes = Changes.create([
+    {
+        first_name: "Eduardito",
+        last_name: "Yubero",
+        email: "edu.yubero@gmail.com",
+        phone: "676412190",
+        contact: contacts.first
+    },
+    {
+        first_name: "Eduardito",
+        last_name: "Yuberito",
+        email: "edu.yubero@gmail.com",
+        phone: "676412190",
+        contact: contacts.first
+    }
+])
